b8de68b41e7f3b2b8fa62d601284a6ca38c83481
whansoo <ghkstn7523@naver.com>
2022-03-02 22:21:16 +0900

    로그인을 요청하는 fetch 생성

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,25 @@
-"use strict";
+"use strict"
 
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+   /* console.log(id);   콘솔로그가 위의id가 실행하기 전에 먼저 실행이 되서 null값이 나옴 */
+                       /*따라서 login.ejs파일에 가서 defer를 추가한다.*/
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+ 
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body:JSON.stringify(req),
+    })
+}
