2266f04a0508fe802951222497fe92adfb84f0fa
whansoo <ghkstn7523@naver.com>
2022-03-02 19:21:02 +0900

    nodemon 적용

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+
 //라우팅
 const routes = require("./routes");
 // 앱 세팅
 app.set("views", "./views");
 app.set("view engine", "ejs");
-
+app.use(express.static(`${__dirname}/public`))
 app.use("/", routes); //use는 미들웨어를 등록해주는 메서드
 
 module.exports = app;
