f4a411f42796720d849d8d4c67e1ed78a1374db0
whansoo <ghkstn7523@naver.com>
2022-03-03 15:25:29 +0900

    백엔드에서 로그인 구현

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 //모듈
 const express = require('express');
 const app = express();
-
+const bodyPaser = require("body-parser");
 
 //라우팅
 const routes = require("./routes");
@@ -10,6 +10,9 @@ const routes = require("./routes");
 app.set("views", "./views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/public`))
+app.use(bodyPaser.json());
+//URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(bodyPaser.urlencoded({extended: true}));
 app.use("/", routes); //use는 미들웨어를 등록해주는 메서드
 
 module.exports = app;
--- public/js/login.js
+++ public/js/login.js
@@ -21,5 +21,8 @@ function login() {
             "Content-Type": "application/json",
         },
         body:JSON.stringify(req),
-    })
+    }).then((res) => res.json());
+       .then((res) =>  {
+
+       });
 }
