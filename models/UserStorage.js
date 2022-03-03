61ee2bf6ba3ddc27c2655dc019e7ff79a6c53684
whansoo <ghkstn7523@naver.com>
2022-03-03 18:45:33 +0900

    user 모델 구현

--- models/UserStorage.js
+++ models/UserStorage.js
@@ -17,5 +17,15 @@ class UserStorage {
    }, {});
    return newUsers;
   }
+  static getUsersInfo(id) {
+      const users = this.#users;
+      const idx = users.id.indexOf(id);
+      const userskeys = Object.keys(users); // =>[id, psword, name]
+      const userInfo = userskeys.reduce((newUser, info) => {
+          newUser[info] = users[info][idx];
+          return newUser;
+      }, {});
+      return userInfo;
+  }
 }
 module.exports = UserStorage;
